969a25dabd6509af2bfe79a76a854ad5d9863028
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-12-16 11:43:53 -0500

    swag

---
+++ public/index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.get("", (req, res) => {
+    res.send("Thami likes bagels and butter.  Also he smells");
+});
+
+app.get("/thamisucks", (req, res) => {
+    res.send("Thami sucks.  And yes I have to do this while you eat.");
+});
+
+app.listen(3000, (req, res) => {
+    console.log('Listening on port 3000');
+});
+
+
+
