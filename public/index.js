18c7d452078e13a8e03b0591ed95681204bd0dbd
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-12-17 11:44:47 -0500

    XD

--- public/index.js
+++ public/index.js
@@ -2,8 +2,22 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-    res.send("Thami likes bagels and butter.  Also he smells");
+/* app.get("", (req, res) => {
+    res.send("Thami 'wasn't' discussing meth");
+}); */
+
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views")
+//set up handlebars engine and views location
+app.set("views engine", "hbs");  //Telling express/NODE to use handlebars for templates
+app.set("views", viewsPath) //telling express to get templates from templates/views folder
+app.get("", async (req, res) => {
+    try {
+        res.render("index");
+    } catch(error) {
+        res.status(500).send();
+    }
 });
 
 app.get("/thamisucks", (req, res) => {
@@ -12,7 +26,9 @@ app.get("/thamisucks", (req, res) => {
 
 app.listen(3000, (req, res) => {
     console.log('Listening on port 3000');
+    console.log(__dirname);
 });
+
 
 
 
