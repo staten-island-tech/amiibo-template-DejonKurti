de4ff17321aaf6f5aa3b1a1bfacf2f17d4423755
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-12-18 11:45:01 -0500

    lk

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
 /* app.get("", (req, res) => {
     res.send("Thami 'wasn't' discussing meth");
@@ -8,14 +9,37 @@ const app = express();
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
 const viewsPath = path.join(__dirname, "../templates/views")
 //set up handlebars engine and views location
-app.set("views engine", "hbs");  //Telling express/NODE to use handlebars for templates
+app.set("view engine", "hbs");  //Telling express/NODE to use handlebars for templates
 app.set("views", viewsPath) //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath);
 app.get("", async (req, res) => {
     try {
-        res.render("index");
-    } catch(error) {
+        res.render("index", {
+            title: "Our first weenie"
+        });
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id;
+    try {
+        res.render("index", {
+         title: `${name}`
+        });
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.get("/swag", async (req, res) => {
+    try {
+        res.render("swag");
+    } catch {
         res.status(500).send();
     }
 });
