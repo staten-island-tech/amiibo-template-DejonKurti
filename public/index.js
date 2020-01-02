b2ce10b3fbf74a1b61f6e5dce52d5a533bebc1eb
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2020-01-02 11:46:37 -0500

    d

--- public/index.js
+++ public/index.js
@@ -1,58 +1,49 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
-const hbs = require("hbs");
-
-/* app.get("", (req, res) => {
-    res.send("Thami 'wasn't' discussing meth");
-}); */
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, "../public");
 const partialsPath = path.join(__dirname, "../templates/partials");
-const viewsPath = path.join(__dirname, "../templates/views")
-//set up handlebars engine and views location
-app.set("view engine", "hbs");  //Telling express/NODE to use handlebars for templates
-app.set("views", viewsPath) //telling express to get templates from templates/views folder
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
 hbs.registerPartials(partialsPath);
+//set up our static asset directory (gimme dat CSS)
+app.use(express.static(publicDirectoryPath));
 app.get("", async (req, res) => {
-    try {
-        res.render("index", {
-            title: "Our first weenie"
-        });
-    } catch {
-        res.status(500).send();
-    }
+  try {
+    res.render("index", {
+      title: "Our First Express App"
+    });
+  } catch {
+    res.status(500).send();
+  }
 });
-
 app.get("/about/:id", async (req, res) => {
-    const name = req.params.id;
-    try {
-        res.render("index", {
-         title: `${name}`
-        });
-    } catch {
-        res.status(500).send();
-    }
+  const name = req.params.id;
+  try {
+    res.render("index", {
+      title: name
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+app.get("/anything", async (req, res) => {
+  try {
+    res.render("swag");
+  } catch {
+    res.status(500).send();
+  }
 });
 
-app.get("/swag", async (req, res) => {
-    try {
-        res.render("swag");
-    } catch {
-        res.status(500).send();
-    }
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+  console.log(viewsPath);
 });
 
-app.get("/thamisucks", (req, res) => {
-    res.send("Thami sucks.  And yes I have to do this while you eat.");
-});
-
-app.listen(3000, (req, res) => {
-    console.log('Listening on port 3000');
-    console.log(__dirname);
-});
-
-
-
+//127.0.0.1:3000
 
